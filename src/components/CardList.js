4b1d54f3d17cfb4d5adc3c1f81ee6105bb702865
Iris Tryba <iristryba@gmail.com>
2022-06-09 19:20:49 +0200

    added ErrerBoundary to ClassList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+    // code for testing Error handling
+    // if (true) {
+    //     throw new Error ('Noooooo');
+    // }
+
     return (
         <div>
             {robots.map((robot) => {
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
@@ -36,8 +37,10 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>   
-                        <CardList robots={ filteredRobots } />
+                    <Scroll>
+                        <ErrorBoundary>   
+                            <CardList robots={ filteredRobots } />
+                        </ErrorBoundary>
                     </Scroll> 
                 </div>
             );
